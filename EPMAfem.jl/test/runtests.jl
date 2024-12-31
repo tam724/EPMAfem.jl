8296dba5e8fedf42e9cfc022700bba59752c08d8
Tamme Claus <tamme.claus@rwth-aachen.de>
2024-12-31 08:22:00 +0100

    add starmap comparison

--- EPMAfem.jl/test/runtests.jl
+++ EPMAfem.jl/test/runtests.jl
@@ -5,4 +5,7 @@ using Test
 @time @testset "SphericalHarmonicsModels" begin include("spherical_harmonics_test.jl") end
 @time @testset "Sparse3Tensor" begin include("sparse3tensor_test.jl") end
 @time @testset "BlockedMatrices" begin include("blockedmatrices_test.jl") end
+@time @testset "OnlyEnergy" begin include("test_only_energy/onlyenergy_test.jl") end
+
+
 end
