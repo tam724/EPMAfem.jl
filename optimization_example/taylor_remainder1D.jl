93b2d809468ad96dba823baf5bcaa6cf7778a677
Tamme Claus <tamme.claus@rwth-aachen.de>
2025-02-06 16:25:59 +0100

    add taylor remainder 1D plot

--- optimization_example/taylor_remainder1D.jl
+++ optimization_example/taylor_remainder1D.jl
@@ -9,37 +9,32 @@ using ComponentArrays
 using LaTeXStrings
 include("../scripts/plot_overloads.jl")
 
-space_model = EPMAfem.SpaceModels.GridapSpaceModel(CartesianDiscreteModel((-1, 0), (100)))
-direction_model = EPMAfem.SphericalHarmonicsModels.EEEOSphericalHarmonicsModel(21, 1)
-model = EPMAfem.DiscretePNModel(space_model, 0.0:0.05:1.0, direction_model)
+space_model = EPMAfem.SpaceModels.GridapSpaceModel(CartesianDiscreteModel((-1, 0), (30)))
+direction_model = EPMAfem.SphericalHarmonicsModels.EEEOSphericalHarmonicsModel(5, 1)
+model = EPMAfem.DiscretePNModel(space_model, 0.0:0.1:1.0, direction_model)
 
 equations = EPMAfem.PNEquations()
 excitation = EPMAfem.PNExcitation([(x=0.0, y=0.0)], [0.8, 0.7, 0.6], normalize.([VectorValue(-1.0, 0.0, 0.0)]))
 extraction = EPMAfem.PNExtraction([0.1, 0.2], equations)
 
 updatable_pnproblem = EPMAfem.discretize_problem(equations, model, EPMAfem.cpu(), updatable=true)
-discrete_system = EPMAfem.schurimplicitmidpointsystem(updatable_pnproblem.problem)
+discrete_system = EPMAfem.schurimplicitmidpointsystem(updatable_pnproblem.problem, eps(Float64); use_direct_solver=true)
 
 discrete_rhs = EPMAfem.discretize_rhs(excitation, model, EPMAfem.cpu())
 discrete_ext = EPMAfem.discretize_extraction(extraction, model, EPMAfem.cpu(), updatable=true)
 
 prob = EPMAfem.EPMAProblem(updatable_pnproblem, discrete_rhs, discrete_ext)
+
 EPMAfem.update_standard_intensities!(prob)
 
 # compute the "true measurements"
 function mass_concentrations(e, x)
-    if x[1] < -0.3
-    # if x[2] > -0.1 && x[2] < 0.25
-    # if sqrt((x[1] + 0.05)^2 + (x[2] - 0.3)^2) < 0.2
-        return e==1 ? 1.0 : 0.0
-    else
-        return e==1 ? 0.0 : 1.0
-    end
+    return e==1 ? 0.4 : 0.6
 end
 true_ρs = EPMAfem.discretize_mass_concentrations([x -> mass_concentrations(e, x) for e in 1:2], model)
 @time true_meas = prob(true_ρs)
 
-# taylor remainder (should be run on CPU..)
+
 function finite_difference_grad(f, p, h)
     val = f(p)
     grad = similar(p)
@@ -57,42 +52,43 @@ objective_function(ρs) = sum((true_meas .- prob(ρs)).^2)# / length(true_meas)
 Nδs = 20
 ρs = similar(true_ρs)
 ρs .= 0.5
-hs = [1/2^i for i in 3:12]
+hs = [1/3^i for i in 0:8]
 taylor_1st = zeros(size(hs)..., Nδs)
 taylor_2nd_ad = zeros(size(hs)..., Nδs)
-taylor_2nd_fd_01 = zeros(size(hs)..., Nδs)
-taylor_2nd_fd_03 = zeros(size(hs)..., Nδs)
+taylor_2nd_fd_02 = zeros(size(hs)..., Nδs)
+taylor_2nd_fd_04 = zeros(size(hs)..., Nδs)
+taylor_2nd_fd_05 = zeros(size(hs)..., Nδs)
+
 grad = Zygote.gradient(objective_function, ρs)
-grad_fd_01 = finite_difference_grad(objective_function, ρs, 1e-1)
-grad_fd_03 = finite_difference_grad(objective_function, ρs, 1e-3)
+
+grad_fd_02 = finite_difference_grad(objective_function, ρs, 1e-2)
+grad_fd_04 = finite_difference_grad(objective_function, ρs, 1e-4)
+grad_fd_05 = finite_difference_grad(objective_function, ρs, 1e-5)
 C = objective_function(ρs)
 
 for n in 1:Nδs
-    δρs = randn(size(ρs)...)
+    δρs = randn(size(ρs)...) |> normalize
     for (i, h) in enumerate(hs)
         @show n, i
         Cδρs = objective_function(ρs + h*δρs)
         taylor_1st[i, n] = abs(Cδρs - C)
         taylor_2nd_ad[i, n] = abs(Cδρs - C - h*dot(grad[1], δρs))
-        taylor_2nd_fd_01[i, n] = abs(Cδρs - C - h*dot(grad_fd_01, δρs))
-        taylor_2nd_fd_03[i, n] = abs(Cδρs - C - h*dot(grad_fd_03, δρs))
+        taylor_2nd_fd_02[i, n] = abs(Cδρs - C - h*dot(grad_fd_02, δρs))
+        taylor_2nd_fd_04[i, n] = abs(Cδρs - C - h*dot(grad_fd_04, δρs))
+        taylor_2nd_fd_05[i, n] = abs(Cδρs - C - h*dot(grad_fd_05, δρs))
     end
 end
 
-plotly()
-plot(grad[1][1, :])
-plot!(grad_fd_01[1, :])
-plot!(grad_fd_03[1, :])
+gr()
+plot(hs, sum(taylor_1st; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="1st rem.", marker=:x, color=1)
+plot!(hs, sum(taylor_2nd_fd_02; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="2nd rem. (FD, Δ=1e-2)", marker=:x, color=3)
+plot!(hs, sum(taylor_2nd_fd_04; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="2nd rem. (FD, Δ=1e-4)", marker=:x, color=4)
+plot!(hs, sum(taylor_2nd_fd_05; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="2nd rem. (FD, Δ=1e-5)", marker=:x, color=5)
+plot!(hs, sum(taylor_2nd_ad; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="2nd rem. (Adjoint)", marker=:x, color=2)
 
-
-
-scatter(hs, sum(taylor_1st; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="1st rem.")
-scatter!(hs, sum(taylor_2nd_ad; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="2nd rem. (Adjoint)")
-scatter!(hs, sum(taylor_2nd_fd_01; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="2nd rem. (FD, Δ=0.1)")
-scatter!(hs, sum(taylor_2nd_fd_03; dims=2)/Nδs, xaxis=:log, yaxis=:log, label="2nd rem. (FD, Δ=0.001)")
-plot!(hs, hs, xaxis=:log, yaxis=:log, color=:gray, ls=:dash, label="1st order")
-plot!(hs, 0.1*hs.^2, xaxis=:log, yaxis=:log, color=:gray, ls=:dashdot, label="2nd order")
+plot!(hs, 1e-2*hs, xaxis=:log, yaxis=:log, color=:gray, ls=:dash, label="1st order")
+plot!(hs, 1e-3*hs.^2, xaxis=:log, yaxis=:log, color=:gray, ls=:dashdot, label="2nd order")
 plot!(size=(400, 300), dpi=1000, legend=:bottomright)
 xlabel!(L"h")
 ylabel!("taylor remainder")
-savefig("figures/epma_taylor_remainder.png")
+savefig("figures/epma_taylor_remainder_direct_1D.png")
